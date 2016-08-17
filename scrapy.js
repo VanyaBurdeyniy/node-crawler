e550ee8c3aa908eeb72e80205bf18654bf3688d0
vanya.burdeyniy <vanya.burdeyniy@do-it.co>
2016-08-17 18:03:35 +0300

    optimize script

--- scrapy.js
+++ scrapy.js
@@ -1,40 +1,42 @@
 'use strict';
 
-var jsdom = require("jsdom");
-var Crawler = require("crawler");
-var url = require('url');
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://188.166.161.9:27017/node-crawler');
+const jsdom = require("jsdom");
+const Crawler = require("crawler");
+const url = require('url');
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost:27017/node-crawler');
 require('./models/content.model');
-var Content = mongoose.model('Content');
+const Content = mongoose.model('Content');
 
 if (!process.argv[2]) throw 'You must specify a web page!';
 
-console.log('Run!!!');
-
-var host = 'https://www.youmagine.com',
+let host = 'http://www.thingiverse.com',
     link = process.argv[2],
     googleTablesData = [],
     allLinksToPages = [],
     count = 0,
     youMaginePageCount = 1,
     thingiVersePageCount = 1,
-    lastPageCount;
+    lastPageCount,
+    arrayCount = 1,
+    isGetAllLinks;
 
 if (process.argv[3] && process.argv[4]) {
     youMaginePageCount = process.argv[3];
     thingiVersePageCount = process.argv[3];
     lastPageCount = process.argv[4];
+} else {
+    isGetAllLinks = true;
 }
 
 if (link.match('thingiverse')) host = 'http://www.thingiverse.com';
 
 
-var c = new Crawler({
+let c = new Crawler({
     maxConnections : 10,
-    callback : function(error, result, $) {
-        $('a').each( function(index, a)  {
-            var toQueueUrl = $(a).attr('href');
+    callback : (error, result, $) => {
+        $('a').each( (index, a) => {
+            let toQueueUrl = $(a).attr('href');
             c.queue(toQueueUrl);
         });
     }
@@ -52,29 +54,34 @@ function youMagineGetAll(url) {
     c.queue([{
         uri: url,
         jQuery: false,
-        callback: function(error, result) {
-            var isGettingContent = false;
+        callback: (error, result) => {
+            let isGettingContent = false;
             jsdom.env(
                 result.body,
                 ["./jquery.js"],
-                function(err, window)  {
-                    window.$.each(window.$('#js-load-more').find('.tile').find('a:first-child'), function(i, v) {
-                        allLinksToPages.push(host + window.$(v).attr('href'));
+                (err, window) => {
+                    window.$.each(window.$('#js-load-more').find('.tile').find('a'), (i, v) => {
+                        if (window.$(v).attr('href') !== '/users/sign_in' && window.$(v).attr('href').match('designs')) allLinksToPages.push(host + window.$(v).attr('href'));
                         isGettingContent = true;
                     });
                     if (isGettingContent) {
                         if (lastPageCount) {
                             if (youMaginePageCount < lastPageCount) {
                                 youMaginePageCount++;
-                                youMagineGetAll('https://www.youmagine.com/designs/latest?page=' + youMaginePageCount + '&state=50');
+                                youMagineGetAll('https://www.youmagine.com/designs/latest?page=' + youMaginePageCount);
                             } else {
                                 getContentFromYouMaginePage();
                             }
                         } else {
-                            youMaginePageCount++;
-                            youMagineGetAll('https://www.youmagine.com/designs/latest?page=' + youMaginePageCount + '&state=50');
+                            if (youMaginePageCount < arrayCount) {
+                                youMaginePageCount++;
+                                youMagineGetAll('https://www.youmagine.com/designs/latest?page=' + youMaginePageCount);
+                            } else {
+                                getContentFromYouMaginePage();
+                            }
                         }
                     } else {
+                        isGetAllLinks = false;
                         getContentFromYouMaginePage();
                     }
                 }
@@ -89,67 +96,74 @@ function getContentFromYouMaginePage() {
     c.queue([{
         uri: allLinksToPages[count],
         jQuery: false,
-        callback: function(error, result) {
+        callback: (error, result) => {
 
             if (allLinksToPages.length == 0) {
-                return console.log('-----------------------\n' +
-                    'Done!');
-            }
+                if (isGetAllLinks) {
+                    arrayCount += 1;
+                    youMaginePageCount++;
+                    youMagineGetAll('https://www.youmagine.com/designs/latest?page=' + youMaginePageCount);
+                } else {
+                    return console.log('-----------------------\n' +
+                        'Done!');
+                }
+            } else {
+                jsdom.env(
+                    result.body,
+                    ["./jquery.js"],
+                    (err, window) => {
+                        let documentId,
+                            pageUrl = allLinksToPages[count],
+                            description = window.$('#information').find('.description').text(),
+                            title = window.$('.design-title').text();
 
-            jsdom.env(
-                result.body,
-                ["./jquery.js"],
-                function(err, window) {
-                    var documentId,
-                        pageUrl = allLinksToPages[count],
-                        description = window.$('#information').find('.description').text(),
-                        title = window.$('#collection-delete-confirmation-dialog').next().text();
-
-                    window.$.each(window.$("#documents").find('.document'), function(i, v) {
-                        if (window.$(v).find('.meta').find('.file-info').text().match('STL')) {
-                            if (documentId) { documentId = documentId + ', ' + host + window.$(v).find('.download').attr('href'); }
-                            else { documentId = host + window.$(v).find('.download').attr('href'); }
-                        }
-                    });
-                    window.$.each(window.$('#js-carousel').find('.images').find('.image'), function(i, v) {
-                        var src = window.$(v).css('background-image');
-                        if (src.match('jpg') || src.match('JPG')) {
+                        window.$.each(window.$("#documents").find('.document'), (i, v) => {
+                            if (window.$(v).find('.meta').find('.file-info').text().match('STL')) {
+                                if (documentId) { documentId = documentId + ', ' + host + window.$(v).find('.download').attr('href'); }
+                                else { documentId = host + window.$(v).find('.download').attr('href'); }
+                            }
+                        });
+                        window.$.each(window.$('#js-carousel').find('.images').find('.image'), (i, v) => {
+                            let src = window.$(v).css('background-image');
                             src = src.replace('"', '');
                             src = src.replace('url(', '');
                             src = src.replace(')', '');
                             googleTablesData.push({
-                                'imageUrl':src,
+                                'imageUrl': 'https://www.youmagine.com' + src,
                                 'title':title,
                                 'description':description,
                                 'pageUrl':pageUrl,
                                 'documentId':documentId
                             });
-                        }
-                    });
+                            if (src.match('jpg') || src.match('JPG')) {
 
-                    if (googleTablesData.length > 0) {
-                        Content.insertMany(googleTablesData);
-                        googleTablesData = [];
-                        if (allLinksToPages.length > 0) {
-                            allLinksToPages.splice(0, 1);
-                            getContentFromYouMaginePage();
+                            }
+                        });
+
+                        if (googleTablesData.length > 0) {
+                            Content.insertMany(googleTablesData);
+                            googleTablesData = [];
+                            if (allLinksToPages.length > 0) {
+                                allLinksToPages.splice(0, 1);
+                                getContentFromYouMaginePage();
+                            } else {
+                                return console.log('-----------------------\n' +
+                                    'Done!');
+                            }
                         } else {
-                            return console.log('-----------------------\n' +
-                                'Done!');
-                        }
-                    } else {
-                        console.log(allLinksToPages.length);
-                        googleTablesData = [];
-                        if (allLinksToPages.length > 0) {
-                            allLinksToPages.splice(0, 1);
-                            getContentFromYouMaginePage();
-                        } else {
-                            return console.log('-----------------------\n' +
-                                'Done!');
+                            console.log(allLinksToPages.length);
+                            googleTablesData = [];
+                            if (allLinksToPages.length > 0) {
+                                allLinksToPages.splice(0, 1);
+                                getContentFromYouMaginePage();
+                            } else {
+                                return console.log('-----------------------\n' +
+                                    'Done!');
+                            }
                         }
                     }
-                }
-            );
+                );
+            }
         }
     }]);
 }
@@ -163,13 +177,13 @@ function thingiVerseGetAll(url) {
     c.queue([{
         uri: url,
         jQuery: false,
-        callback: function(error, result) {
-            var isGettingContent = false;
+        callback: (error, result) => {
+            let isGettingContent = false;
             jsdom.env(
                 result.body,
                 ["./jquery.js"],
-                function(err, window) {
-                    window.$.each(window.$('.things-page').find('.thing-img-wrapper'), function(i, v) {
+                (err, window) => {
+                    window.$.each(window.$('.things-page').find('.thing-img-wrapper'), (i, v) => {
                         allLinksToPages.push(host + window.$(v).attr('href'));
                         isGettingContent = true;
                     });
@@ -182,10 +196,15 @@ function thingiVerseGetAll(url) {
                                 getContentFromThingiVersePage();
                             }
                         } else {
-                            youMaginePageCount++;
-                            thingiVerseGetAll('http://www.thingiverse.com/ajax/things/paginate_things?page='+thingiVersePageCount);
+                            if (thingiVersePageCount < arrayCount) {
+                                thingiVersePageCount++;
+                                thingiVerseGetAll('http://www.thingiverse.com/ajax/things/paginate_things?page='+thingiVersePageCount);
+                            } else {
+                                getContentFromThingiVersePage();
+                            }
                         }
                     } else {
+                        isGetAllLinks = false;
                         getContentFromThingiVersePage();
                     }
                 }
@@ -200,60 +219,66 @@ function getContentFromThingiVersePage() {
     c.queue([{
         uri: allLinksToPages[count],
         jQuery: false,
-        callback: function(error, result) {
+        callback: (error, result) => {
 
             if (allLinksToPages.length == 0) {
-                return console.log('-----------------------\n' +
-                    'Done!');
-            }
+                if (isGetAllLinks) {
+                    arrayCount += 1;
+                    thingiVersePageCount++;
+                    thingiVerseGetAll('http://www.thingiverse.com/ajax/things/paginate_things?page='+thingiVersePageCount);
+                } else {
+                    return console.log('-----------------------\n' +
+                        'Done!');
+                }
+            } else {
+                jsdom.env(
+                    result.body,
+                    ["./jquery.js"],
+                    (err, window) => {
+                        let documentId,
+                            pageUrl = allLinksToPages[count],
+                            description = window.$('.thing-component-header.summary').next().text(),
+                            title = window.$('.thing-page-header').first().find('.thing-header-data').find('h1').text();
 
-            jsdom.env(
-                result.body,
-                ["./jquery.js"],
-                function(err, window) {
-                    var documentId,
-                        pageUrl = allLinksToPages[count],
-                        description = window.$('.thing-component-header.summary').next().text(),
-                        title = window.$('.thing-page-header').first().find('.thing-header-data').find('h1').text();
+                        window.$.each(window.$(".thing-file-download-link"), (i, v) => {
+                            if (documentId) { documentId = documentId + ', ' + host + window.$(v).attr('href'); }
+                            else { if (window.$(v).attr('data-file-name').match('STL') || window.$(v).attr('data-file-name').match('stl')) documentId = host + window.$(v).attr('href'); }
+                        });
+                        window.$.each(window.$('.thing-gallery-thumb'), (i, v) => {
+                            if (window.$(v).attr('data-large-url').match('jpg') || window.$(v).attr('data-large-url').match('JPG')) {
+                                googleTablesData.push({
+                                    'imageUrl':window.$(v).attr('data-large-url'),
+                                    'title':title,
+                                    'description':description,
+                                    'pageUrl':pageUrl,
+                                    'documentId':documentId
+                                });
+                            }
+                        });
 
-                    window.$.each(window.$(".thing-file-download-link"), function(i, v) {
-                        if (documentId) { documentId = documentId + ', ' + host + window.$(v).attr('href'); }
-                        else { if (window.$(v).attr('data-file-name').match('STL') || window.$(v).attr('data-file-name').match('stl')) documentId = host + window.$(v).attr('href'); }
-                    });
-                    window.$.each(window.$('.thing-gallery-thumb'), function(i, v) {
-                        if (window.$(v).attr('data-large-url').match('jpg') || window.$(v).attr('data-large-url').match('JPG')) {
-                            googleTablesData.push({
-                                'imageUrl':window.$(v).attr('data-large-url'),
-                                'title':title,
-                                'description':description,
-                                'pageUrl':pageUrl,
-                                'documentId':documentId
-                            });
-                        }
-                    });
-
-                    if (googleTablesData.length > 0) {
-                        Content.insertMany(googleTablesData);
-                        googleTablesData = [];
-                        if (allLinksToPages.length > 0) {
-                            allLinksToPages.splice(0, 1);
-                            getContentFromThingiVersePage();
+                        if (googleTablesData.length > 0) {
+                            Content.insertMany(googleTablesData);
+                            googleTablesData = [];
+                            if (allLinksToPages.length > 0) {
+                                allLinksToPages.splice(0, 1);
+                                getContentFromThingiVersePage();
+                            } else {
+                                return console.log('-----------------------\n' +
+                                    'Done!');
+                            }
                         } else {
-                            return console.log('-----------------------\n' +
-                                'Done!');
-                        }
-                    } else {
-                        googleTablesData = [];
-                        if (allLinksToPages.length > 0) {
-                            allLinksToPages.splice(0, 1);
-                            getContentFromThingiVersePage();
-                        } else {
-                            return console.log('-----------------------\n' +
-                                'Done!');
+                            googleTablesData = [];
+                            if (allLinksToPages.length > 0) {
+                                allLinksToPages.splice(0, 1);
+                                getContentFromThingiVersePage();
+                            } else {
+                                return console.log('-----------------------\n' +
+                                    'Done!');
+                            }
                         }
                     }
-                }
-            );
+                );
+            }
         }
     }]);
 }
